494ec5a724b2af72a87e5ac26e323090fbbcd46c
Better-By-01 <patelsinghashutosh@gmail.com>
2022-02-19 18:33:14 +0530

    Day 6

---
+++ Day6/learn.py
@@ -0,0 +1,20 @@
+# Functions
+def my_function():          # defining a function
+    print("Hello")
+    print("Bye")
+
+my_function()               # calling a function
+    # Hello
+    # Bye
+
+# While loops
+# while something_is_true:
+# You know it
+
+
+#negation
+a = 0 
+while not a:                # while(!a) # while(!0) # while(1) for one time only
+    print("Hello World")
+
+# prints hello world only one time
