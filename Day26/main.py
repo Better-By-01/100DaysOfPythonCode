5734cbe2c772f96e4fd7d06299f5db86356e5c9e
Better-By-01 <patelsinghashutosh@gmail.com>
2022-03-09 08:23:54 +0530

    KeyError exception handled for characters other than alphabets

--- Day26/main.py
+++ Day26/main.py
@@ -2,8 +2,22 @@ import pandas
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 code_dict = {row.letter:row.code for (index, row) in df.iterrows()}
-word = input("Enter a word: ").upper()
-coded_word = [code_dict[letter] for letter in word]
-print(coded_word)
+
+def generated_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        coded_word = [code_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabets")
+        generated_phonetic()
+    else:
+        print(coded_word)
+
+generated_phonetic()
+
+
 # Enter a word: Ashutosh
 # ['Alfa', 'Sierra', 'Hotel', 'Uniform', 'Tango', 'Oscar', 'Sierra', 'Hotel']
+
+# Enter a word: ashutosh32
+# Sorry, only letters in the alphabet
